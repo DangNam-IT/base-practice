e4329d3e21e8e83cac5854e7a02a599551f1121f
Dang Phuong Nam <dangphuongnam102004@gmail.com>
2026-04-15 09:28:24 +0700

    [update]: refactor for database method

--- src/app/database.py
+++ src/app/database.py
@@ -1,28 +1,31 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 
 from app.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL_SYNC,
-    future=True,
-    pool_pre_ping=True,
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False, 
-    autoflush=False, 
-    bind=engine, 
-    class_=Session
-)
-
 Base = declarative_base()
+_engine = None
+_SessionLocal = None
+def get_engine():
+    global _engine
+    if _engine is None:
+        connect_args = {"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
+        _engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
+    return _engine
 
+
+def get_session_local():
+    global _SessionLocal
+    if _SessionLocal is None:
+        _SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=get_engine(), class_=Session)
+    return _SessionLocal
+
+
+# Giữ lại engine và SessionLocal như property để không break code cũ
+engine = get_engine()
 
 def get_db():
-    """Dependency: cung cấp DB session, tự đóng sau mỗi request."""
-    db = SessionLocal()
+    db = get_session_local()()
     try:
         yield db
     finally:
